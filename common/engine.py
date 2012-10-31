d56a203be36e9b501f4abbd8c37e5e37cd3a2e24
binkydd <stef@stef.local>
2012-10-31 23:26:01 +0100

    baseclass engine

---
+++ common/engine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+aiplayer.py
+
+Created by Stef Bastiaansen & Bart Desmet on 2012-10-31.
+"""
+
+from common import grid
+
+class Engine(object):
+	
+	def startRun():
+		self.grid = new Grid()
+
+
+
+
+
