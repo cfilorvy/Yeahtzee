577a9856b67bfaff91c0852c494d5f66cf7758d2
Bart Desmet <bart@Barts-MacBook-Pro.local>
2012-10-31 22:17:06 +0100

    Replaced deprecated optparse library with argparse and added some 1337 features for passing engines on the command line

--- common/gerard.py
+++ common/gerard.py
@@ -30,31 +30,67 @@ def get_hit_pct(grids, position):
     return sum(1 for grid in grids if grid.return_score_or_zero(position))/float(len(grids))
 
 
-def main(processes = 1, runs = 100, store_grids = False):
+def solo(engine, threads = 1, games = 100, store_grids = False):
     grids = [] # Deze lijst wordt nogal groot na verloop van tijd, omdat er volledige grids in worden opgeslagen. Met 10000 runs was dat 50MB, dus met 1 miljoen runs (voor 1 engine) is dat al 5GB. Gelukkig heb ik sinds kort 16 GB RAM, dus we kunnen 2 engines een miljoen spelletjes tegen elkaar laten spelen, no problem. Bovendien is het opslaan van volledige grids enkel nuttig voor het testen van engines, kwestie van statistische analyses te kunnen doen op de performance. Bij het effectieve battlen moeten we enkel de grand totals onthouden.
-    if processes == 1: # Running sequentially
+    if threads == 1: # Running sequentially
         runcounter = 0
-        for run in range(runs):
-            runcounter += 1
-            if runcounter % 1000 == 0:
-                print runcounter, "of", runs, "games played"
-            grids.append(automahtzee.main(1, True))
-    elif 1 < processes < 33: # Multiprocessing pwnz0r!!
-        pool = Pool(processes)
-        grids = pool.map(automahtzee.main, range(runs))
+        for game in range(games):
+            gamecounter += 1
+            if gamecounter % 1000 == 0:
+                print gamecounter, "of", games, "games played"
+            grids.append(engine.main(1, True))
+    elif 1 < threads < 33: # Multiprocessing pwnz0r!!
+        pool = Pool(threads)
+        grids = pool.map(engine.main, range(games))
     else:
-        print >>sys.stderr, "Too many processes:", processes
+        print >>sys.stderr, "Too many processes:", threads
         sys.exit(1)
     
     print "Minimum, average and maximum grand total:", get_min(grids, "gt"), get_avg(grids, "gt"), get_max(grids, "gt")
     print "Yahtzee hit percentage:", get_hit_pct(grids, "yz")
     print "Yahtzee bonus hit percentage:", get_hit_pct(grids, "yb")
-    # print sum(grids)/float(runs)
+    # print sum(grids)/float(games)
+
+
+def CLParser():
+    '''Parses the command line arguments and returns them.'''
+    import argparse
+    parser = argparse.ArgumentParser(description = "Let's play some Yeahtzee!", epilog = "And that, my friends, is how it's done.")
+    parser.add_argument("-t", "--threads", type = int, default = 4, help="Maximum number of simultaneous threads")
+    parser.add_argument("-g", "--games", type = int, default = 100, help="Number of games to be played")
+    parser.add_argument("-e", "--engines", nargs = "*", default = [], help="Engines participating in the tournament. Options for an engine can be appended with dots.")
+    arguments = parser.parse_args()
+    
+    try: threads = int(arguments.threads)
+    except ValueError:
+        print >>sys.stderr, "Enter a valid number for option -t"
+        sys.exit(1)
+    
+    try: games = int(arguments.games)
+    except ValueError:
+        print >>sys.stderr, "Enter a valid number for option -g"
+        sys.exit(1)
+    
+    try: assert arguments.engines
+    except AssertionError:
+        print >>sys.stderr, "Specify engines using the -e option"
+        sys.exit(1)
+    
+    engines = []
+    for engine_caller in arguments.engines:
+        engine_fields = engine_caller.split(".")
+        try:
+            engine_module = __import__("engines.%s" % engine_fields[0], fromlist=["engines"])
+            engine = (engine_module, engine_fields[1:])
+            engines.append(engine)
+        except ImportError:
+            print >>sys.stderr, "Could not find engine module '%s'" % engine_fields[0]
+            sys.exit(1)
+    print engines
+    
+    return threads, games, engines
 
 
 if __name__ == "__main__":
-    try: processes = int(sys.argv[1])
-    except IndexError: processes = 4
-    try: runs = int(sys.argv[2])
-    except IndexError: runs = 1000
-    main(processes, runs, store_grids = False)
+    threads, games, engines = CLParser()
+    solo(engines[0][0], threads, games, store_grids = False)
