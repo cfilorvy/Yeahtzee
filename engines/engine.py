69ef16a643745b81b2dffe27a07011868bb15ead
Megasnort <stef@megasnort.com>
2014-02-22 16:05:02 +0100

    Moved engine.py

---
+++ engines/engine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Engine base class
+
+Created by Stef Bastiaansen & Bart Desmet on 2012-10-31.
+"""
+
+from common import grid
+
+class Engine(object):
+	
+	def __init__(self):
+		self.grid = grid.Grid()
+
+
+	def start_to_run_complete(self):
+		while self.grid.available_positions():
+			self.turn(verbose);
+
+		return self.grid
+
+
+	def start_to_run_turn(self):
+		self.turn();	
+		return grid;
+		
+
+
+
+
+
